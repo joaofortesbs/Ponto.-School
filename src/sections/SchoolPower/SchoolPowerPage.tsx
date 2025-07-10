f1a6d4feb2e106026b73a9203a0bf8244eb29396
João Fortes <44872185-joaomarcelofort@users.noreply.replit.com>
2025-07-10 21:20:17 +0000

    Restored to 'd1cb25f1135999e40858d60a0206dbc4fb01951d'
    Replit-Restored-To: d1cb25f1135999e40858d60a0206dbc4fb01951d

--- src/sections/SchoolPower/SchoolPowerPage.tsx
+++ src/sections/SchoolPower/SchoolPowerPage.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import ParticlesCanvas from './components/ParticlesCanvas';
 import AIMessageBox from './components/AIMessageBox';
+import AvatarCentral from './components/AvatarCentral';
 
 export function SchoolPowerPage() {
   return (
     <div className="min-h-screen relative overflow-hidden" style={{ background: 'transparent' }}>
-      <div className="absolute top-20 left-0 right-0 bottom-0">
-        <ParticlesCanvas />
+      <ParticlesCanvas />
+      <div className="absolute -top-8 left-1/2 transform -translate-x-1/2 z-10 pointer-events-auto">
+        <AvatarCentral />
       </div>
       <div className="absolute bottom-32 left-1/2 transform -translate-x-1/2 z-10">
         <AIMessageBox />
