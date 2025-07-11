64bf1a543ff1fe0ceaa5a5a867f3910ffdd1de8c
João Fortes <44877729-joaomarcelofort@users.noreply.replit.com>
2025-07-11 02:08:40 +0000

    Restored to '00e3488371232b99c3d3ffd61e9f59a685202a3b'
    Replit-Restored-To: 00e3488371232b99c3d3ffd61e9f59a685202a3b

--- src/sections/SchoolPower/SchoolPowerPage.tsx
+++ src/sections/SchoolPower/SchoolPowerPage.tsx
@@ -1,35 +1,19 @@
-
 import React from 'react';
-import Header from '@/components/layout/Header';
-import Sidebar from '@/components/layout/Sidebar';
 import ParticlesCanvas from './components/ParticlesCanvas';
 import AIMessageBox from './components/AIMessageBox';
 import AvatarCentral from './components/AvatarCentral';
 
 export function SchoolPowerPage() {
   return (
-    <div className="min-h-screen bg-gray-50 dark:bg-[#001427]">
-      {/* Header fixo */}
-      <Header />
-      
-      <div className="flex">
-        {/* Sidebar fixa */}
-        <Sidebar />
-        
-        {/* Conteúdo principal da School Power */}
-        <main className="flex-1 ml-64 pt-[72px]">
-          <div className="relative h-[calc(100vh-72px)] overflow-hidden" style={{ background: 'transparent' }}>
-            <ParticlesCanvas>
-              <div className="absolute -top-8 left-1/2 transform -translate-x-1/2 z-10 pointer-events-auto">
-                <AvatarCentral />
-              </div>
-              <div className="absolute bottom-4 left-1/2 transform -translate-x-1/2 z-10">
-                <AIMessageBox />
-              </div>
-            </ParticlesCanvas>
-          </div>
-        </main>
-      </div>
+    <div className="min-h-screen relative overflow-hidden" style={{ background: 'transparent' }}>
+      <ParticlesCanvas>
+        <div className="absolute -top-8 left-1/2 transform -translate-x-1/2 z-10 pointer-events-auto">
+          <AvatarCentral />
+        </div>
+        <div className="absolute bottom-4 left-1/2 transform -translate-x-1/2 z-10">
+          <AIMessageBox />
+        </div>
+      </ParticlesCanvas>
     </div>
   );
 }
