2f4c65e8830d6c9b86e6ee449dd16cc69dfc2b4d
João  Fortes <45033935-joaomarcelofor1@users.noreply.replit.com>
2025-07-17 05:46:25 +0000

    Restored to '8ec50426313a136b3a737886b9076f12a3b285a2'
    Replit-Restored-To: 8ec50426313a136b3a737886b9076f12a3b285a2

--- vite.config.ts
+++ vite.config.ts
@@ -1,29 +1,24 @@
+
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
+import { componentTagger } from "lovable-tagger";
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
+export default defineConfig(({ command }) => ({
+  server: {
+    host: "::",
+    port: 8080,
+  },
+  plugins: [
+    react(),
+    command === 'serve' && componentTagger(),
+  ].filter(Boolean),
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  optimizeDeps: {
-    exclude: ['lucide-react'],
+  esbuild: {
+    target: 'esnext',
   },
-  css: {
-    postcss: {
-      plugins: [
-        require('tailwindcss/nesting'),
-        require('tailwindcss'),
-        require('autoprefixer'),
-      ],
-    },
-  },
-  server: {
-    host: '0.0.0.0',
-    port: 8080,
-  },
-});
+}));
