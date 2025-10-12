aa7ba3fe88ba5ab40a558188114ce62885651200
João Fortes <joaomarcelofortesempresa@gmail.com>
2025-10-12 23:19:27 +0000

    Restored to 'd8b78e7c55d2cbfa6ac28056d3bb739000626297'
    Replit-Restored-To: d8b78e7c55d2cbfa6ac28056d3bb739000626297

--- src/components/sidebar/SidebarNav.tsx
+++ src/components/sidebar/SidebarNav.tsx
@@ -1142,7 +1142,7 @@ export function SidebarNav({
           justify-content: center;
           flex-shrink: 0 !important;
           margin-left: auto;
-          margin-right: 20px;
+          margin-right: 12px;
         }
 
         .lock-icon i {
